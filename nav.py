a466115bc76b6463cb5a83d5890fd1e506bb8d11
Pikachu <daniel_han2008@hotmail.com>
2026-08-14 16:39:36 +0800

    Update nav.py

--- nav.py
+++ nav.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import streamlit as st
-
+from supabase import create_client   
 import gspread
 from google.oauth2.service_account import Credentials
 from datetime import datetime
